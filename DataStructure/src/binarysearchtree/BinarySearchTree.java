768ddb082900c2a15c57de70032cde466e7eef22
Wafaa Saber <42408429+Wafaas@users.noreply.github.com>
2024-04-08 06:52:49 +0200

    Add files via upload

--- DataStructure/src/binarysearchtree/BinarySearchTree.java
+++ DataStructure/src/binarysearchtree/BinarySearchTree.java
@@ -37,6 +37,18 @@ public class BinarySearchTree {
                 temp = temp.right;
             }
         }
+    }
 
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while(temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else return true;
+        }
+        return false;
     }
 }
